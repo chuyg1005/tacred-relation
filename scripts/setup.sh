a09aa718ce3317e692e957f597925722cd9b1c58
yukang <ygchu7@163.com>
2024-04-25 13:39:00 +0800

    fix softlink create bugs

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@ data_root="../data"
 results_root="../results"
 # 下载glove词向量
 rm -rf dataset/glove
-ln -s ${data_root}/glove dataset/glove
+ln -s ../${data_root}/glove dataset/glove
 if [ ! -d "${data_root}/glove" ]; then
    echo "Downloading GloVe"
    mkdir ${data_root}/glove
@@ -15,8 +15,8 @@ rm -rf dataset/tacred
 rm -rf dataset/tacred-vocab
 rm -rf dataset/retacred
 rm -rf dataset/retacred-vocab
-ln -s ${data_root}/tacred dataset/tacred
-ln -s ${data_root}/retacred dataset/retacred
+ln -s ../${data_root}/tacred dataset/tacred
+ln -s ../${data_root}/retacred dataset/retacred
 
 rm -rf saved_models
 if [ ! -d "${results_root}/${model_name}" ]; then
@@ -35,11 +35,11 @@ rm -rf saved_models/retacred
 # 创建VOCAB
 if [ ! -d "${results_root}/${model_name}/dataset/tacred-vocab" ]; then
     mkdir ${results_root}/${model_name}/dataset/tacred-vocab
-    ln -s ${results_root}/${model_name}/dataset/tacred-vocab dataset/tacred-vocab
+    ln -s ../${results_root}/${model_name}/dataset/tacred-vocab dataset/tacred-vocab
     python prepare_vocab.py dataset/tacred dataset/tacred-vocab --glove_dir dataset/glove
 fi
 if [ ! -d "${results_root}/${model_name}/dataset/retacred-vocab" ]; then
     mkdir ${results_root}/${model_name}/dataset/retacred-vocab
-    ln -s ${results_root}/${model_name}/dataset/retacred-vocab dataset/retacred-vocab
+    ln -s ../${results_root}/${model_name}/dataset/retacred-vocab dataset/retacred-vocab
     python prepare_vocab.py dataset/retacred dataset/retacred-vocab --glove_dir dataset/glove
 fi
