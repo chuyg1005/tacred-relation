2d9ca5d756b68145c3ddc5e25c8b471e980e6e32
yukang <ygchu7@163.com>
2024-04-25 12:15:11 +0800

    update filepath refers in scripts/setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,12 @@
 model_name="tacred-relation"
+data_root="../data"
+results_root="../results"
 # 下载glove词向量
 rm -rf dataset/glove
-ln -s /home/data_91_d/chuyg/mix-debias/glove dataset/glove
-if [ ! -d "/home/data_91_d/chuyg/mix-debias/glove" ]; then
+ln -s ${data_root}/glove dataset/glove
+if [ ! -d "${data_root}/glove" ]; then
    echo "Downloading GloVe"
-   mkdir /home/data_91_d/chuyg/mix-debias/glove
+   mkdir ${data_root}/glove
    bash scripts/download.sh
 fi
 
@@ -13,31 +15,31 @@ rm -rf dataset/tacred
 rm -rf dataset/tacred-vocab
 rm -rf dataset/retacred
 rm -rf dataset/retacred-vocab
-ln -s /home/data_91_d/chuyg/mix-debias/data/tacred dataset/tacred
-ln -s /home/data_91_d/chuyg/mix-debias/data/retacred dataset/retacred
+ln -s ${data_root}/tacred dataset/tacred
+ln -s ${data_root}/retacred dataset/retacred
 
 rm -rf saved_models
-if [ ! -d "/home/data_91_d/chuyg/mix-debias/${model_name}" ]; then
-    mkdir /home/data_91_d/chuyg/mix-debias/${model_name}
+if [ ! -d "${results_root}/${model_name}" ]; then
+    mkdir ${results_root}/${model_name}
 fi
-if [ ! -d "/home/data_91_d/chuyg/mix-debias/${model_name}/saved_models" ]; then
-    mkdir /home/data_91_d/chuyg/mix-debias/${model_name}/saved_models
+if [ ! -d "${results_root}/${model_name}/saved_models" ]; then
+    mkdir ${results_root}/${model_name}/saved_models
 fi
-if [ ! -d "/home/data_91_d/chuyg/mix-debias/${model_name}/dataset" ]; then
-    mkdir /home/data_91_d/chuyg/mix-debias/${model_name}/dataset
+if [ ! -d "${results_root}/${model_name}/dataset" ]; then
+    mkdir ${results_root}/${model_name}/dataset
 fi
-ln -s /home/data_91_d/chuyg/mix-debias/${model_name}/saved_models saved_models
+ln -s ${results_root}/${model_name}/saved_models saved_models
 rm -rf saved_models/tacred
 rm -rf saved_models/retacred
 
 # 创建VOCAB
-if [ ! -d "/home/data_91_d/chuyg/mix-debias/${model_name}/dataset/tacred-vocab" ]; then
-    mkdir /home/data_91_d/chuyg/mix-debias/${model_name}/dataset/tacred-vocab
-    ln -s /home/data_91_d/chuyg/mix-debias/${model_name}/dataset/tacred-vocab dataset/tacred-vocab
+if [ ! -d "${results_root}/${model_name}/dataset/tacred-vocab" ]; then
+    mkdir ${results_root}/${model_name}/dataset/tacred-vocab
+    ln -s ${results_root}/${model_name}/dataset/tacred-vocab dataset/tacred-vocab
     python prepare_vocab.py dataset/tacred dataset/tacred-vocab --glove_dir dataset/glove
 fi
-if [ ! -d "/home/data_91_d/chuyg/mix-debias/${model_name}/dataset/retacred-vocab" ]; then
-    mkdir /home/data_91_d/chuyg/mix-debias/${model_name}/dataset/retacred-vocab
-    ln -s /home/data_91_d/chuyg/mix-debias/${model_name}/dataset/retacred-vocab dataset/retacred-vocab
+if [ ! -d "${results_root}/${model_name}/dataset/retacred-vocab" ]; then
+    mkdir ${results_root}/${model_name}/dataset/retacred-vocab
+    ln -s ${results_root}/${model_name}/dataset/retacred-vocab dataset/retacred-vocab
     python prepare_vocab.py dataset/retacred dataset/retacred-vocab --glove_dir dataset/glove
 fi
